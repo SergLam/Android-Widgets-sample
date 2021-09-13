31eb108606487738404799e4bf76ea799e9945e5
SergLam <lamcevs0@gmail.com>
2021-09-13 11:53:36 +0300

    project clean up

--- app/src/main/java/learn/tasha/widgetexample/SecondActivity.java
+++ app/src/main/java/learn/tasha/widgetexample/SecondActivity.java
@@ -1,8 +1,8 @@
 package learn.tasha.widgetexample;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Created by tashariko on 26/08/17.
